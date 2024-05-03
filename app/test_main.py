98f43caeac13f63681805d3fceadf1b99288d773
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-05-03 11:19:06 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,39 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents,error",
+    [
+        ("23", TypeError),
+        (23.1, TypeError),
+        (-2, ValueError),
+    ],
+    ids=[
+        "Should raise an error when string given.",
+        "Should raise an error when float given.",
+        "Should raise an error when negative given."
+    ]
+)
+def test_should_raise_error(cents: int, error: type[Exception]) -> None:
+    with pytest.raises(error):
+        get_coin_combination(cents)
+
+
+@pytest.mark.parametrize(
+    "cents,expected",
+    [
+        (0, [0, 0, 0, 0]),
+        (4, [4, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (9, [4, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (19, [4, 1, 1, 0]),
+        (24, [4, 0, 2, 0]),
+        (25, [0, 0, 0, 1]),
+        (68, [3, 1, 1, 2]),
+    ]
+)
+def test_should_return_coins(cents: int, expected: list) -> None:
+    assert get_coin_combination(cents) == expected
